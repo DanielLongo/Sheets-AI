98c7c316f688366185e04c70e9a6bf166c3e61e6
DanielLongo <danieldominiclongo@gmail.com>
2023-01-01 02:57:31 -0800

    loom video added

--- react-webapp/src/pages/Landing.js
+++ react-webapp/src/pages/Landing.js
@@ -5,8 +5,8 @@ function Landing() {
   return (
     <div>
       <section>
-          <div className="p-20 justify-center bg-slate-50 flex-1 flex h-screen">
-            <div className="w-full rounded-md p-16 flex flex-col justify-center items-center h-4/6">
+          <div className="p-20 justify-center bg-slate-50 flex-1 flex h-screen flex-col">
+            <div className="w-full rounded-md p-16 flex flex-col justify-center items-center h-2/6">
               <h1
                 className="font-CerealXBd lg:text-5xl text-3xl mb-6 bg-gradient-to-r bg-clip-text text-transparent
                         from-emerald-500 via-indigo-500 to-emerald-500 font-bold
@@ -20,6 +20,11 @@ function Landing() {
               <button className="bg-blue-800 py-2 px-4 text-blue-50 rounded-lg mb-4" onClick={() => navigate("/home")}>
                 Get Started
               </button>
+            </div>
+            <div>
+            </div>
+            <div className="w-full h-full items-center justify-center flex">
+            <iframe className="w-[555px] h-[333px]" src="https://www.loom.com/embed/2021a3c573624235ba4407e2c1d625c6" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
             </div>
           </div>
         </section>
