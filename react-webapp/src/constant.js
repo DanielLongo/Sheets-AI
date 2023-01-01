9a4e95d3473a80377f99ac6c0ce0c0d9e358c7af
DanielLongo <danieldominiclongo@gmail.com>
2023-01-01 02:31:40 -0800

    endpoint change

--- react-webapp/src/constant.js
+++ react-webapp/src/constant.js
@@ -1,2 +1,2 @@
-// export const BACKEND_ENDPOINT = "https://sheets-ai-ysdnuplvtq-uc.a.run.app";
-export const BACKEND_ENDPOINT = "http://localhost:8000";
+export const BACKEND_ENDPOINT = "https://sheets-ai-ysdnuplvtq-uc.a.run.app";
+// export const BACKEND_ENDPOINT = "http://localhost:8000";
